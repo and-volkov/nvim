258b9c4942df0e4c5119f4ef49ca146d0b1f0b4a
Andrey Volkov <andrey.dreamwall@gmail.com>
2023-12-30 23:53:22 +0400

    Add setup file for neogit

---
+++ after/plugin/neogit.lua
@@ -0,0 +1,27 @@
+local neo = require("neogit")
+
+neo.setup({
+	disable_signs = false,
+	disable_context_highlighting = false,
+	disable_commit_confirmation = false,
+	auto_refresh = true,
+	disable_builtin_notifications = false,
+	commit_popup = {
+		kind = "split",
+	},
+	-- customize displayed signs
+	signs = {
+		-- { CLOSED, OPENED }
+		section = { ">", "v" },
+		item = { ">", "v" },
+		hunk = { "", "" },
+	},
+	integrations = {
+		diffview = true,
+	},
+	-- override/add mappings
+	mappings = {
+		-- modify status buffer mappings
+		status = {},
+	},
+})
